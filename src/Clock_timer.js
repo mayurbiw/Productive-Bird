8776ef8622b3f382a7403068d85285144603a95d
mayurbiw <mayurbiw@gmail.com>
2019-08-13 19:54:19 +0530

    changes

--- src/Clock_timer.js
+++ src/Clock_timer.js
@@ -5,17 +5,17 @@ import './Clock_timer.css';
 
 class Clock_timer extends React.Component {
 
-
-
 constructor(props){
 	super(props);
 
 	this.state =  {
 	
+	timer: 50,
 	timerHour : 0,
 	timerMinute : 50,
 	timerSec : 0,
-	label : '50'
+	task:""	
+
 
 	
 	};
@@ -27,21 +27,21 @@ constructor(props){
 	
 	if (event.target.getAttribute('data-min') == 25) {
 		
-			this.setState({timerMinute:25,timerHour:0,timerSec : 0 ,label:'25'});
+			this.setState({timerMinute:25,timerHour:0,timerSec : 0 ,timer:25});
 			event.target.className ='btn_selected';
 			document.getElementById('time50').className='btn';
 			document.getElementById('time90').className='btn';
 	}
 
 	if(event.target.getAttribute('data-min') == 50){
-			this.setState({timerMinute:50, timerHour:0, timerSec : 0 ,  label:'50'});
+			this.setState({timerMinute:50, timerHour:0, timerSec : 0 ,timer:50});
 			event.target.className ='btn_selected';
 			document.getElementById('time25').className='btn';
 			document.getElementById('time90').className='btn';
 	}
 
 	if(event.target.getAttribute('data-min') == 90){
-			this.setState({timerMinute:30,  timerHour:1,timerSec : 0  ,label:'1 : 30'});
+			this.setState({timerMinute:2,  timerHour:1,timerSec : 0  ,timer:90});
 			event.target.className ='btn_selected';
 			document.getElementById('time25').className='btn';
 			document.getElementById('time50').className='btn';
@@ -67,7 +67,7 @@ return(
 
 <div>
 
-<input type="text" name="task" placeholder="Task"/>
+<input type="text" name="task" placeholder="Task" value = {this.state.task} onChange = {this.updateTask}/>
 </div>
 
 <div className="Time_div_margin">
@@ -77,7 +77,11 @@ return(
 
 
 <div className="action_btns">
-<button class="Play" onClick = {this.Start}>Start</button>
+
+<button className="Play" onClick = {this.Start}>Start</button>
+<button className="Stop" onClick = {this.Stop}>Stop</button>
+<button className="Reset" disabled = {true} onClick = {this.Reset}>Reset</button>
+
 </div>
 </div>
 
@@ -85,13 +89,59 @@ return(
 
 }
 
+updateTask = (event) => {
 
-Start = () => {
+this.setState({task:event.target.value});
+
+}
+
+
+Start = (event) => {
+
+
+/*	Replace start with stop 
+	Show reset button
+	
+
+
+
+*/
+
+event.target.disabled = true;
+document.querySelector(".Reset").disabled=false;
+event.target.style.display = 'none';
+document.querySelector(".Stop").style.display = 'inline-block';
 
 var self = this;
-
-setInterval(function(){
+var start = event;
+var refreshId =  setInterval(function(){
   
+
+	if(self.state.timerHour == 0 && self.state.timerMinute  == 0 && self.state.timerSec == 0){
+		
+		/*	save data locally 
+			save graph data points 
+			enable start 
+			disable stop 
+	*/
+		clearInterval(refreshId);
+		 // saveTask();
+
+
+
+	}
+
+	if(self.state.timerMinute>0 && self.state.timerSec==0){
+		let  new_timerMinute =  self.state.timerMinute - 1 
+		self.setState({timerMinute:new_timerMinute});
+	}
+	
+	if(self.state.timerHour>0 && self.state.timerMinute==0){
+		let  new_timerHour =  self.state.timerHour - 1
+		let new_minute = 60;
+		self.setState({timerHour:new_timerHour,timerMinute:new_minute});
+	}
+	
 	if (self.state.timerSec==0)
 			self.setState({timerSec:60});
 
@@ -104,6 +154,16 @@ setInterval(function(){
 
 
 
+saveTask = () => {
+
+var value = ["25","50","90"]
+localStorage.setItem(this.state.task, JSON.stringify(value));
+var test = JSON.parse(localStorage.getItem("tast"));
+alert(test);
+
 }
 
+
+
+}
 export default Clock_timer;
